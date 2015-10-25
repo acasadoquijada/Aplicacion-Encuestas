d5aa50ebbc7c2360f6a0901c41a4b32b1535f125
acasadoquijada <acasadoquijada@gmail.com>
2015-10-25 13:04:59 +0100

    Update

---
+++ pollaplication/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pollaplication',
+      version='0.1',
+      description='Web application about polls',
+      url='https://github.com/acasadoquijada/pollaplication',
+      author='Alejandro Casado Quijada',
+      author_email='acasadoquijada@gmail.com',
+      license='GNU GPL',
+      packages=['django','wheel'],
+      zip_safe=False)
