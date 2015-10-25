f7f8c2ad0f89c637ae9c8838fbf6cb93787d0e16
acasadoquijada <acasadoquijada@gmail.com>
2015-10-25 13:15:07 +0100

    Corregido error

--- pollaplication/setup.py
+++ pollaplication/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='pollaplication',
-      version='0.1',
-      description='Web application about polls',
-      url='https://github.com/acasadoquijada/pollaplication',
-      author='Alejandro Casado Quijada',
-      author_email='acasadoquijada@gmail.com',
-      license='GNU GPL',
-      packages=['django','wheel'],
-      zip_safe=False)
+	version='0.1',
+	description='Web application about polls',
+	url='https://github.com/acasadoquijada/pollaplication',
+	author='Alejandro Casado Quijada',
+	author_email='acasadoquijada@gmail.com',
+	license='GNU GPL',
+	packages=['pollaplication'],
+	install_requires=['django','wheel'],
+	zip_safe=False)
